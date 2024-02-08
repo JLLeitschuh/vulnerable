d9eb4be55a669ca1547d1c18d8e79cdb357d498e
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2024-02-07 19:42:52 -0500

    Add valid vulnerability sink
    Signed-off-by: Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>

--- newest/src/main/java/org/jlleitschuh/vulnerable/ApacheMultipartFilename.java
+++ newest/src/main/java/org/jlleitschuh/vulnerable/ApacheMultipartFilename.java
@@ -7,15 +7,18 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.List;
 
 public class ApacheMultipartFilename {
-    public void handleCommonsFileUpload(HttpServletRequest request) throws FileUploadException {
+    public void handleCommonsFileUpload(HttpServletRequest request) throws FileUploadException, FileNotFoundException {
         DiskFileItemFactory factory = new DiskFileItemFactory();
         ServletFileUpload servletFileUpload = new ServletFileUpload(factory);
         List<FileItem> items = servletFileUpload.parseRequest(request);
         for (FileItem item : items) {
             File f = new File("uploadDir", item.getName());
+            FileOutputStream os = new FileOutputStream(f);
         }
     }
 }
--- newest/src/main/java/org/jlleitschuh/vulnerable/ApacheMultipartFilenameJakarta.java
+++ newest/src/main/java/org/jlleitschuh/vulnerable/ApacheMultipartFilenameJakarta.java
@@ -6,16 +6,19 @@ import org.apache.commons.fileupload2.core.FileUploadException;
 import org.apache.commons.fileupload2.jakarta.servlet6.JakartaServletFileUpload;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.List;
 
 public class ApacheMultipartFilenameJakarta {
 
-    public void handleCommonsFileUploadJakarta(jakarta.servlet.http.HttpServletRequest request) throws FileUploadException {
+    public void handleCommonsFileUploadJakarta(jakarta.servlet.http.HttpServletRequest request) throws FileUploadException, FileNotFoundException {
         DiskFileItemFactory factory = DiskFileItemFactory.builder().get();
         JakartaServletFileUpload<DiskFileItem, DiskFileItemFactory> servletFileUpload = new JakartaServletFileUpload<>(factory);
         List<DiskFileItem> items = servletFileUpload.parseRequest(request);
         for (DiskFileItem item : items) {
             File f = new File("uploadDir", item.getName());
+            FileOutputStream os = new FileOutputStream(f);
         }
     }
 }
